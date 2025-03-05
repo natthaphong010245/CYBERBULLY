089158912b9e34dec44eb772eb1257be42d64c2f
__b_2002 <natthaphong010245@gmail.com>
2025-03-05 14:37:13 +0700

    Define: layouts home&index

--- resources/views/layouts/login&register/footer/login.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="fixed bottom-0 left-0 w-full bg-white text-center py-2.5 z-10">
-    <p class="text-sm text-gray-700">
-        @yield('footer-text')
-    </p>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Main pages
 Route::get('/', function () {
     return view('index');
 });
+
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/main', function () {
     return view('main');
-});
+})->name('main');
+
+Route::get('/login', function () {
+    return view('login&register.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('login&register.register');
+})->name('register');
+
+Route::get('/main', function () {
+    return view('main');
+})->name('main');
